f5981f411d1b5e40ae0535180c1432b6e1773fb7
Newname <newemail>
2013-07-09 02:30:18 +0000

    using hello world from index.html now

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
-var express = require('express');
 
+var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var index = new Buffer(fs.readFileSync('index.html'));
+    response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
